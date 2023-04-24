3bb7781e667ccc92451ebdc8842b8c4240fb007f
meloinu <meloinuofficial>
2023-04-23 22:26:33 -0600

    man

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,7 @@ import '../styles/globals.css'
 const MyApp = ({ Component, pageProps }) => {
     return (
         <ThirdwebProvider
-            desiredChainId={ChainId.Rinkeby}
-            chainRpc={{
-                [ChainId.Rinkeby]: 'https://rinkeby.infura.io/v3/e656989154c944b3927d928c063e33eb',
-            }}
+            activeChainId={ChainId.binance}
         >
             <Component {...pageProps} />
         </ThirdwebProvider>
